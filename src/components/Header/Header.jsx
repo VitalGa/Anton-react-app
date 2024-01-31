d4edc90c24e4e7d19204a3a24770aed4c4d89191
VitalGa <vitalga10@gmail.com>
2024-01-31 11:20:37 +0300

    how to work batching

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react';
 import Button from '../Button/Button';
-import SelectUser from '../SelectUser/SelectUser';
-import styles from './Header.module.css';
+import SelectUser from '../SelectUser/SelectUser'; 
+import Logo from '../Logo/Logo';
 
 const logos = ['/logo.svg', '/vite.svg'];
 
 function Header() {
   const [logoIndex, setLogoIndex] = useState(0);
-
-  const toggleLogo = () => {
+  const [secontIndex, setSecontIndex] = useState(0);
+  console.log(secontIndex);
+  const toggleLogo =  () => {
     setLogoIndex(state => Number(!state));
-  }
+    setSecontIndex(i => i + 1);
+  };
 
   return (
     <>
-      <img className={styles.logo} src={logos[logoIndex]} alt="Логотип журнала" />
+      <Logo image={logos[logoIndex]}/>
       <SelectUser />
       <Button onClick={toggleLogo}>Сменить лого</Button>
     </>    
